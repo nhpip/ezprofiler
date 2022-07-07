b99683eefb5aa70480d496e963f57277efcfc980
Matt Evans <81327996+nhpip@users.noreply.github.com>
2022-07-06 22:09:23 -0400

    wip

--- priv/dep_clean.exs
+++ priv/dep_clean.exs
@@ -1,11 +1,3 @@
-defmodule DepClean do
-
-  def clean() do
-
-  end
-
-end
  
-IO.puts("#{Path.relative_to_cwd(Mix.Project.deps_path())}")
-IO.puts("#{Path.dirname(File.cwd!())}")
-IO.puts("#{Mix.Project.deps_path()}")
+if Path.dirname(File.cwd!()) == Mix.Project.deps_path(), 
+    do: File.rm_rf!("#{Mix.Project.build_path()}/lib/ezprofiler/")
