dfc539c7ffaefdd8af0b177dcd5823eeeb7e531f
Matt Evans <81327996+nhpip@users.noreply.github.com>
2022-07-08 14:39:38 -0400

    wip

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EZProfiler.MixFile do
   def project do
     [
       app: :ezprofiler,
-      version: "1.1.1",
+      version: "1.2.0",
       elixir: "~> 1.11",
       escript: escript(),
       package: package(),
