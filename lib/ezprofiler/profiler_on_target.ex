f6048fc5a510ebcd89c1fdb8a97d88fecf884c4a
Matt Evans <81327996+nhpip@users.noreply.github.com>
2022-07-18 16:33:43 -0400

    wip

--- lib/ezprofiler/profiler_on_target.ex
+++ lib/ezprofiler/profiler_on_target.ex
@@ -419,7 +419,7 @@ defmodule EZProfiler.ProfilerOnTarget do
   end
 
   @doc false
-  def handle_event({:call, from}, :code_stop, :profiling, %{cp_started: _, label_transition: label_transition?, current_labels: current_labels, profiler_node: profiler_node, current_results_filename: filename, code_manager_pid: cpid, profiler: profiler} = state) do
+  def handle_event({:call, from}, :code_stop, :profiling, %{label_transition: label_transition?, current_labels: current_labels, profiler_node: profiler_node, current_results_filename: filename, code_manager_pid: cpid, profiler: profiler} = state) do
     profiling_complete(:code_stop, state)
     result_str = make_final_results(:code_stop, state)
                  |> finalize_results_file(state)
